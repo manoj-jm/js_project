012f23ecc7e23e0e5d3973a0effd5ebc3b16ace7
manoj <manojjmvr49@gmail.com>
2023-11-14 18:08:22 +0530

    added comment

--- js_weather_project/script.js
+++ js_weather_project/script.js
@@ -13,7 +13,7 @@ async function checkWeather(city) {
   }
 
   var data = await response.json();
-  //   console.log(data);
+    // console.log(data);
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
